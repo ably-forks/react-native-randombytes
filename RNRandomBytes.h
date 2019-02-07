b9619b66142d77ad61e27c32fd7637dc2eee4937
jonathan schatz <jon@divisionbyzero.com>
2019-02-07 11:42:44 -0800

    React native 0.56+ support
    port of https://github.com/mvayngrib/react-native-randombytes/pull/28/files

--- RNRandomBytes.h
+++ RNRandomBytes.h
@@ -7,8 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "RCTBridgeModule.h"
-#import "RCTBridge.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include(“RCTBridgeModule.h”)
+#import “RCTBridgeModule.h”
+#else
+#import “React/RCTBridgeModule.h” // Required when used as a Pod in a Swift project
+#endif
+
 
 @interface RNRandomBytes : NSObject<RCTBridgeModule>
 
